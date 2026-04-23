9297652ef20c9bd83130e1872bbc763c9bf4b980
Justin-225 <happing51409@gmail.com>
2026-04-23 23:15:43 +0800

    2ndUpdate FirstClass20Team.java

--- FirstClass20Team.java
+++ FirstClass20Team.java
@@ -2,5 +2,6 @@ public class FirstClass20Team {
   public static void main(String[] args) {
     System.out.println("這是用來練習GitHub協作的程式!");
     System.out.println("\"學號:\"01357020\"\n姓名:\"詹貴帆\"");
+    System.out.println("\"學號:\"01357109\"\n姓名:\"王晉紳\"");
   }
 }
